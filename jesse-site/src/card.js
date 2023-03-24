b12c09ba4900fb60a7db401e811db616506eb04f
unknown <bolanosjjr@gmail.com>
2023-03-23 21:31:09 -0500

    pics component

---
+++ jesse-site/src/card.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import pics from './pics.js';
+
+
+
---
+++ jesse-site/src/pics.js
@@ -0,0 +1,25 @@
+const pics = [
+
+    {
+        id: 1,
+        name: "Boxing Bag",
+        imgURL:"C:/Users/Owner/visual code/Jesse/jesse-site/src/pics/bag and fighter.jpg"
+    },
+    {
+        id: 2,
+        name: "Crypto Arena",
+        imgURL:"C:/Users/Owner/visual code/Jesse/jesse-site/src/pics/jesse crypto.jpg"
+    },
+    {
+        id: 3,
+        name: "champ win",
+        imgURL:"C:/Users/Owner/visual code/Jesse/jesse-site/src/pics/champ green.jpg"
+    },
+    {
+        id: 4,
+        name: "logo",
+        imgURL:"Jesse/jesse-site/src/pics/beast.jpg"
+    }
+]
+
+export default pics;
